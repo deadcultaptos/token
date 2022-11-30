ad0e09ce9587d9fe46c33382514bf43ccaf0527e
deadcultaptos <aptosdeadcult@gmail.com>
2022-11-30 15:11:14 +0100

    add buttons

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,8 +14,8 @@ function Hero() {
             strings={['winning 🥇', 'earning', 'participate 🫡', 'burning 🔥']} typeSpeed={40} backSpeed={50} loop />
           </div>
           <div className='space-x-5 pt-10 font-bold'>
-            <a href="" target="_blank"><button className='border text-bold py-3 px-5 rounded-md bg-maincolor border-maincolor text-gray-900 hover:bg-white'>BUY $FWCI</button></a>
-            <a href="" target="_blank"><button className='border text-bold py-3 px-5 rounded-md text-maincolor hover:text-white'>$FWCI CHART</button></a>
+            <a href="https://app.uniswap.org/#/swap?outputCurrency=0x7fe7C4163DdfFb928e5B89D64959d0a06040641a" target="_blank"><button className='border text-bold py-3 px-5 rounded-md bg-maincolor border-maincolor text-gray-900 hover:bg-white'>BUY $FWCI</button></a>
+            <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xd60d64f1974e4a3d610581cc244ac155eab90ec1" target="_blank"><button className='border text-bold py-3 px-5 rounded-md text-maincolor hover:text-white'>$FWCI CHART</button></a>
           </div>
     </div>
   )
